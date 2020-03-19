18a8c9c43c487867a6371aba8a32c380f99d5b5a
serlon <1218714931@qq.com>
2020-03-20 07:35:16 +0800

    修复resetbug,加入持创观测

--- ENV.py
+++ ENV.py
@@ -2,7 +2,9 @@
 import multiprocessing
 import os, time, random
 import pandas as pd
+import numpy as np
 from catalyst import run_algorithm
+
 from catalyst.api import (order_target_percent, symbol)
 
 NAMESPACE = 'ltc_DDPG'
@@ -12,6 +14,7 @@ START_DATE = pd.to_datetime('2019-3-6', utc=True)
 END_DATE = pd.to_datetime('2019-3-7', utc=True)
 # 观测历史条数
 BAR_COUNT = 5
+
 
 class LTCENV():
     def reset(self):
@@ -32,11 +35,13 @@ class LTCENV():
         })
         # 开启框架进程
         self.p1.start()
-        # 执行第一步
-        self.pipe[1].send(0)
+        # 发送reset只反回观测，不执行动作
+        self.pipe[1].send("reset")
+
         # 等响应
         respone = self.pipe[1].recv()
-        return respone[0]
+
+        return respone
 
     def __initialize(self, context):
         context.i = 1
@@ -44,29 +49,45 @@ class LTCENV():
         context.set_commission(maker=0.002, taker=0.001)  # 设置手续费
 
     def __handle_data(self, context, data):
+        # 观测历名数据
+        short_data = data.history(context.asset,
+                                  ('open', 'close', 'high', 'low', 'volume'),
+                                  bar_count=BAR_COUNT,
+                                  frequency="1m", )
+
+        history = short_data.values
+
+        context.std = np.std(history, axis=0)
+        context.mean = np.mean(history, axis=0)
+        context.observation = (history - context.mean) / context.std
+        context.observation = context.observation.reshape(-1)
+        if context.i == 1:
+            # 当为1时，前面执行的是reset,所以反回当前观测
+            context.observation = np.insert(context.observation,len(context.observation),0)
+            self.pipe[0].send(context.observation)
         if context.i > 1:
             # 执行完动作后发送相关观测
             ltc_value = data.current(context.asset, "price") * float(context.portfolio.positions[context.asset].amount)
             value = ltc_value + float(context.portfolio.cash)
             # 当前资产-现在资产
             rewards = value - context.value
+            #加入当前持仓
+            context.observation = np.insert(context.observation, len(context.observation), ltc_value/value)
             self.pipe[0].send([context.observation, rewards, False])
 
         # 接收动作并执行动作
         action = self.pipe[0].recv()
+        if action == "reset":
+            # 如果是重置的情况，再等下一个动作吧
+            action = self.pipe[0].recv()
+
         order_target_percent(context.asset, action)
-        short_data = data.history(context.asset,
-                                  ('open', 'close', 'high', 'low', 'volume'),
-                                  bar_count=BAR_COUNT,
-                                  frequency="1m", )
-        context.observation = short_data.values
 
         ltc_value = data.current(context.asset, "price") * float(context.portfolio.positions[context.asset].amount)
         context.value = ltc_value + float(context.portfolio.cash)
         context.i += 1
 
     def __analyze(self, context, perf):
-
         self.pipe[0].send([context.observation, 0, True])
         pass
 
--- demo.py
+++ demo.py
@@ -4,11 +4,13 @@ env = LTCENV()
 
 observation = env.reset()
 
+
 while True:
+
     action = random.uniform(-1, 1)
     observation_, r, Done = env.step(action)
     print(observation)
-    print(r)
     if Done:
         print("完了")
         break
+    observation = observation_
